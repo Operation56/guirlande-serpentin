20dc633ed2adb3db42a930d817afd6a160d3a9dd
Operation56 <99143278+Operation56@users.noreply.github.com>
2022-02-07 08:32:37 +0100

    Fonctionnementt OK

--- main.ts
+++ main.ts
@@ -1,12 +1,12 @@
 let colonne_droite_vers_gauche = 0
 basic.forever(function () {
     for (let Ligne = 0; Ligne <= 4; Ligne++) {
-        for (let Colonne = 0; Colonne <= 4; Colonne++) {
-            colonne_droite_vers_gauche = 4 + Colonne
+        for (let colonne_gauche_vers_droite = 0; colonne_gauche_vers_droite <= 4; colonne_gauche_vers_droite++) {
+            colonne_droite_vers_gauche = 4 - colonne_gauche_vers_droite
             if (Ligne % 2 == 0) {
-                led.plot(Colonne, Ligne)
+                led.plot(colonne_gauche_vers_droite, Ligne)
                 basic.pause(300)
-                led.unplot(Colonne, Ligne)
+                led.unplot(colonne_gauche_vers_droite, Ligne)
             } else {
                 led.plot(colonne_droite_vers_gauche, Ligne)
                 basic.pause(300)
